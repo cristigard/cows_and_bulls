54557de8fd195b468e04115b94473018a8c293e5
Cristi Gard <65028018+cristigard@users.noreply.github.com>
2021-11-25 00:08:44 +0200

    Update cows_and_bulls.py

--- cows_and_bulls.py
+++ cows_and_bulls.py
@@ -1,26 +1,45 @@
 import random
-def cow_bull(user_number, random_number):
-    cows_bulls =[0,0]
-    for i in range(len(random_number)):
-        if user_number[i] == random_number[i]:
-            cows_bulls[0] += 1
-        elif user_number[i] in random_number:
-            cows_bulls[1] += 1
-    return cows_bulls
+#generate 4 digit random number
+number=random.randint(1000,9999)
 
-playing=True
-random_number=str(random.randint(1000,9999))
-guessed=0
-while playing:
-    guessed+=1
-    print(random_number)
-    user_number=input(str("Type number from 1000 to 9999: "))
-    result = cow_bull(user_number,random_number)
-    if result[0]<4:
-        print("Cows: ", result[0], "Bulls:",result[1])
+#func to convert list from int
+def from_str_to_list(str_num):
+    str_num=str(str_num)
+    lst_num=[x for x in str_num]
+    return lst_num
+
+# convert random number to list
+random_number_list=from_str_to_list(number)
+
+cow=0
+tries=0
+
+while cow<4:
+
+    #tracking how many tries user perform
+    tries+=1
+
+    # reset cow,bull for every trie
+    cow = 0
+    bull = 0
+
+    # User input 4 digit number
+    user_input = input('4 digit number: ')
+
+    # convert user input number to list
+    user_inuput_list = from_str_to_list(user_input)
+
+    # engine
+    for index,value in enumerate(random_number_list):
+            if random_number_list[index]==user_inuput_list[index]:
+                cow+=1
+            elif random_number_list[index] in user_inuput_list:
+                bull+=1
     else:
-        if guessed==1:
-             print("You guessed the number:",random_number, "from first time")
-        else:
-            print("You guessed the number:",random_number, "from", guessed, "times")
-        playing=False
+        print('Cow:',cow)
+        print('Bull:',bull)
+else:
+    if tries==1:
+        print("You made it from first trie!")
+    else:
+        print("You made it from",tries,"tries!")
